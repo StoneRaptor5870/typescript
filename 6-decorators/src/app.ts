0930ed10b0f335ad72f2e392bf20fb6dda017170
Nischay <nischay.dtu@gmail.com>
2024-03-23 00:58:28 +0530

    returning class in a class decorator, autobind

--- 6-decorators/src/app.ts
+++ 6-decorators/src/app.ts
@@ -1,1 +1,61 @@
 // returning ( and changing ) a class in a class decorator
+function WTemplate(template: string, hookId: string) {
+  console.log('TEMPLATE FACTORY');
+  return function<T extends {new(...args: any[]): {name: string}}>(originalConstructor: T) {
+    return class extends originalConstructor {
+      constructor(...args: any[]) {
+        super();
+        console.log('Rendering template');
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector('h1')!.textContent = this.name;
+        }
+      }
+    }
+  }
+}
+
+@WTemplate('<h1>My Person Object</h1>', 'app')
+class Stuff {
+  name = 'Nischay';
+
+  constructor() {
+    console.log('Creating person object...');
+  }
+}
+
+const x = new Stuff();
+console.log(x);
+
+function Autobind(target: any, methodName: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  };
+  return adjDescriptor;
+}
+
+// creating an autobind decorator
+class Printer {
+  message: string;
+
+  constructor() {
+    this.message = 'This works!';
+  }
+
+  @Autobind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const r = new Printer();
+r.showMessage();
+const button = document.querySelector('button')!;
+button.addEventListener('click', r.showMessage);
--- 6-decorators/src/decoratorsBasics.ts
+++ 6-decorators/src/decoratorsBasics.ts
@@ -115,4 +115,5 @@ class Product {
   }
 }
 
-
+const p1 = new Product('Book', 19);
+const p2 = new Product('Book 2', 29);

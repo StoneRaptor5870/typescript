434c283e0c15650b6bc58fd7f074c9cc5c8c4e2e
Nischay <nischay.dtu@gmail.com>
2024-03-23 21:25:14 +0530

    Rendering Projects Lists

--- 7-drag-n-drop/src/app.ts
+++ 7-drag-n-drop/src/app.ts
@@ -41,6 +41,35 @@ function autobind(target: any, method: string, descriptor: PropertyDescriptor){
   return adjDescriptor;
 }
 
+//Project List Class
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: 'active' | 'finished') {
+    this.templateElement = <HTMLTemplateElement>document.getElementById('project-list')!;
+    this.hostElement = <HTMLDivElement>document.getElementById('app')!;
+
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = <HTMLElement>importedNode.firstElementChild;
+    this.element.id = `${this.type}-projects`;
+
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector('ul')!.id = listId;
+    this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + 'PROJECTS';
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('beforeend', this.element);
+  }
+}
+
 // Project Input Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -124,3 +153,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');

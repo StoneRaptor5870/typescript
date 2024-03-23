06c9ff7364d5a5a20c1461864b4506ec62e8d2f2
Nischay <nischay.dtu@gmail.com>
2024-03-23 19:27:32 +0530

    Interacting with DOM Elements

--- 7-drag-n-drop/src/app.ts
+++ 7-drag-n-drop/src/app.ts
@@ -1,8 +1,10 @@
-// DOM Element Selection & OOP Rendering
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement
 
   constructor() {
     this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!;
@@ -10,7 +12,23 @@ class ProjectInput {
 
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = <HTMLFormElement>importedNode.firstElementChild;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler.bind(this));
   }
 
   private attach() {
